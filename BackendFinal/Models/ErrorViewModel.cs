c71362961e35e9efa748bfb421641026ce70d008
unknown <abulfazfa@code.edu.az>
2023-06-24 23:35:39 +0400

    pushToGithub

---
+++ BackendFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackendFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
